4f14b23b8b7a25d2a6cd288b5fa2424c5f97ef0a
André Fiedler <fiedler@gewamusic.com>
2026-02-25 20:50:01 +0100

    feat: update version to 1.3.40 and implement DrawProgressSmoother for improved remaining time estimation

--- src/AppVersion.mjs
+++ src/AppVersion.mjs
@@ -2,7 +2,7 @@
  * Current application version synced from package.json.
  */
 export class AppVersion {
-    static #current = '1.3.39'
+    static #current = '1.3.40'
 
     /**
      * Returns current application version.
